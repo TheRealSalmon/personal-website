ed7038663f8732689a683406933ac8fe582d2e8e
Sam Mun <therealsam@berkeley.edu>
2024-06-14 11:43:54 -0700

    Update home.rs

--- src/home.rs
+++ src/home.rs
@@ -3,7 +3,7 @@ use leptos::*;
 #[component]
 pub fn Home() -> impl IntoView {
     view! {
-      <p class="text-xl absolute bottom-12 right-12">
+      <p class="text-lg absolute bottom-0">
         "
         Jack-of-all-trades with broad experience across computational drug
         discovery. Deep expertise in custom virtual screening workflows and
