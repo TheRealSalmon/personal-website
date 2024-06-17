37ec81f9f28705b1978c53f5381cb6d1c29b81f1
Sam Mun <therealsam@berkeley.edu>
2024-06-17 10:21:13 -0700

    delete unnecessary lib.rs

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![allow(non_snake_case)]
-
-pub mod app;
-pub mod articles;
-pub mod blog;
-pub mod contact;
-pub mod home;
-pub mod projects;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+#![allow(non_snake_case)]
+
+pub mod app;
+pub mod articles;
+pub mod blog;
+pub mod contact;
+pub mod home;
+pub mod projects;
+
+use crate::app::App;
 use leptos::{mount_to_body, view};
-use personal_website::app::App;
 
 fn main() {
     console_error_panic_hook::set_once();
