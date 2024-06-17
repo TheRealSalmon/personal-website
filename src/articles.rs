6390cf0f900cb85e2bb1a5741e8f180bcd5ae8cb
Sam Mun <therealsam@berkeley.edu>
2024-06-17 10:12:34 -0700

    create Article component

--- src/articles.rs
+++ src/articles.rs
@@ -3,37 +3,48 @@
 use leptos::*;
 
 #[component]
-pub fn WatchingMyselfRecover() -> impl IntoView {
-    let lines = include_str!("../articles/watching-myself-recover.txt").split("\n\n");
+fn Article(title: String, date: String, lines: Vec<String>) -> impl IntoView {
     view! {
       <br/>
       <br/>
       <br/>
       <br/>
-      <p class="text-base italic">"Watching Myself Recover"</p>
-      <p class="text-sm italic">"June 17th, 2024"</p>
+      <p class="text-base italic">{&title}</p>
+      <p class="text-sm italic">{&date}</p>
 
       {lines
-          .into_iter()
+          .iter()
           .map(|line| view! { <p class="text-base sm:text-xl pt-6">{line}</p> })
           .collect_view()}
     }
 }
 
 #[component]
-pub fn LivingWithCancer() -> impl IntoView {
-    let lines = include_str!("../articles/living-with-cancer.txt").split("\n\n");
+pub fn WatchingMyselfRecover() -> impl IntoView {
+    let lines: Vec<_> = include_str!("../articles/watching-myself-recover.txt")
+        .split("\n\n")
+        .map(|s| s.to_string())
+        .collect();
     view! {
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <p class="text-base italic">"Living with Cancer"</p>
-      <p class="text-sm italic">"June 13th, 2024"</p>
+      <Article
+        title="Watching Myself Recover".to_string()
+        date="June 17th, 2024".to_string()
+        lines=lines
+      />
+    }
+}
 
-      {lines
-          .into_iter()
-          .map(|line| view! { <p class="text-base sm:text-xl pt-6">{line}</p> })
-          .collect_view()}
+#[component]
+pub fn LivingWithCancer() -> impl IntoView {
+    let lines: Vec<_> = include_str!("../articles/living-with-cancer.txt")
+        .split("\n\n")
+        .map(|s| s.to_string())
+        .collect();
+    view! {
+      <Article
+        title="Living with Cancer".to_string()
+        date="June 13th, 2024".to_string()
+        lines=lines
+      />
     }
 }
