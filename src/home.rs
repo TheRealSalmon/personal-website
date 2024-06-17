d79658934ba5e0b3fa9306a07c2178548acac383
Sam Mun <therealsam@berkeley.edu>
2024-06-17 10:22:10 -0700

    Update home.rs
    on non-mobile, use w-1/4 for home text to look cool

--- src/home.rs
+++ src/home.rs
@@ -3,7 +3,7 @@ use leptos::*;
 #[component]
 pub fn Home() -> impl IntoView {
     view! {
-      <p class="text-lg absolute bottom-0 right-0">
+      <p class="sm:w-1/4 text-lg absolute bottom-0 right-0">
         "
         Jack-of-all-trades with broad experience across computational drug
         discovery. Deep expertise in custom virtual screening workflows and
