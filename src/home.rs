04a963027fdf63cb5a752f1fa32c986bdc7e38ca
Sam Mun <therealsam@berkeley.edu>
2024-06-18 11:28:47 -0700

    Update home.rs
    change breakpoint

--- src/home.rs
+++ src/home.rs
@@ -3,7 +3,7 @@ use leptos::*;
 #[component]
 pub fn Home() -> impl IntoView {
     view! {
-      <p class="sm:w-1/4 text-lg absolute bottom-0 right-0">
+      <p class="lg:w-1/3 text-lg absolute bottom-0 right-0">
         "
         Jack-of-all-trades with broad experience across computational drug
         discovery. Deep expertise in custom virtual screening workflows and
